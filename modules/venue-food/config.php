e7df46c7fdc6b39e28000fef89aec5ccfcc7c7a7
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-02-14 12:11:25 +0700

    Add q field

--- modules/venue-food/config.php
+++ modules/venue-food/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'venue-food',
-    '__version' => '0.0.1',
+    '__version' => '0.0.2',
     '__git' => 'git@github.com:getmim/venue-food.git',
     '__license' => 'MIT',
     '__author' => [
--- modules/venue-food/model/VenueFood.php
+++ modules/venue-food/model/VenueFood.php
@@ -14,5 +14,5 @@ class VenueFood extends \Mim\Model
 
     protected static $chains = [];
 
-    protected static $q = [];
+    protected static $q = ['name'];
 }
